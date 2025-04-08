78b2819a4674be1f2a5dca9d0d61998e1199cd42
leo <ingrogger@gmail.com>
2025-04-08 20:17:33 -0300

    applying some layout improvement

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import './layout.css';
 
 function Layout() {
-  return (
-    <div>
-      <header>
-        <h1>BBJS-REACTJS</h1>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/hook1-rotatingbox">Hook example 1</a></li>
-            <li><a href="/phy1-falling-ball">Physic example1</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+
+        <div className="wrapper">
+            <header className="main-head">Showroom project</header>
+            <nav className="main-nav">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/hook1-rotatingbox">Hook example 1</a></li>
+                    <li><a href="/phy1-falling-ball">Physic example1</a></li>
+                </ul>
+            </nav>
+            <main className="content">
+                <Outlet />
+            </main>
+
+            <footer className="main-footer">leostereo</footer>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
 }
 
 export default Layout;
